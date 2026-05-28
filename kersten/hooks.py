b27108297fdbf2f50a1e8603410a5b2503c3b23a
Chris Martin <csmartin.mail@gmail.com>
2026-05-28 15:13:28 +0100

    Add workspace files to Hooks

--- kersten/hooks.py
+++ kersten/hooks.py
@@ -192,6 +192,9 @@ doc_events = {
 # }
 fixtures = [
     {"dt": "Custom Field","filters": [["module", "=","kersten"]]},
+	{"dt": "Workspace Sidebar", "filters": [["name", "in", ["Kersten Website", "Kersten CRM"]]]},
+    {"dt": "Desktop Icon", "filters": [["app", "=", "kersten"]]},
+    {"dt": "Workspace", "filters": [["app", "=", "kersten"]]},
 ]
 # Testing
 # -------
