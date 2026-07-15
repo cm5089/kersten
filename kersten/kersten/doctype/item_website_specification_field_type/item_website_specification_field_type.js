83b201e286b2d2c5df2f4c5c9288cb61dfdda045
Chris Martin <csmartin.mail@gmail.com>
2026-07-15 12:43:47 +0000

    Add auto-generated files to Doctype Item Website Specification Field Type and reformat JSON template.

---
+++ kersten/kersten/doctype/item_website_specification_field_type/item_website_specification_field_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, viral and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Item Website Specification Field Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
