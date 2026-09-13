a777115c94c389e75e8d9ac9812a6e49d25a118e
Chris Martin <csmartin.mail@gmail.com>
2026-09-13 19:38:49 +0000

    refactor: repalced has_related_tems with get_related_items_by_group to hit the database less

--- kersten/kersten/override/website_item.py
+++ kersten/kersten/override/website_item.py
@@ -35,16 +35,17 @@ class WebsiteItem(_WebsiteGenerator):
 	def has_long_description(self):
 		return frappe.utils.strip_html(self.web_long_description or '') != ''
 
-	def has_recommended_items(self, group=None):
-		published_recommended_items = [
-			item for item in self.recommended_items if frappe.db.get_value("Website Item", item.website_item, "published")
-			]
-		has_recommended_items = published_recommended_items and len(published_recommended_items) > 0
-		if group:
-			has_recommended_items = has_recommended_items and any(
-				item.custom_group == group for item in published_recommended_items
-			)
-		return has_recommended_items
+	def get_recommended_items_by_group(self):
+		published_recommended_items = self.filter_unpublished_website_items(self.recommended_items)
+		grouped_items = {}
+		for item in published_recommended_items:
+			if item.custom_group not in grouped_items.keys():
+				grouped_items[item.custom_group] = []
+			grouped_items[item.custom_group].append(item)
+		return grouped_items
+
+	def filter_unpublished_website_items(self, items):
+		return [item for item in items if frappe.db.get_value("Website Item", item.website_item, "published")]
 
 	def get_tabs(self):
 		tab_values = {}
